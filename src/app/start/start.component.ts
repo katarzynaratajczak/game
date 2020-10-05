a8806da78f220b5e390c0686f414ace1eecddc12
Katarzyna <ksulzycka@gmail.com>
2020-10-05 16:42:47 +0200

    dodanie animacji do StartComponent

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
+start = '/assets/images/start/0.png';
+
+
   constructor() { }
 
   ngOnInit() {
